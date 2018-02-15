e7cd57a19ed857113e0b961527ac557d796eaa0a
Yonghao Zhang <31497431+SilentsingerZ@users.noreply.github.com>
2018-02-14 23:30:47 -0700

    upload final version of INFO4602 Project 1

---
+++ ZHANG/project.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $("#partOne").show();
+  $("#partTwo").hide();
+  $("#partThree").hide();
+  $("#partFive").hide();
+  $("#partSix").hide();
+
+  $("#part1").click(function() {
+    $("#partOne").show();
+    $("#partTwo").hide();
+    $("#partThree").hide();
+    $("#partFive").hide();
+    $("#partSix").hide();
+  });
+
+  $("#part2").click(function() {
+    $("#partTwo").show();
+    $("#partOne").hide();
+    $("#partThree").hide();
+    $("#partFive").hide();
+    $("#partSix").hide();
+  });
+
+  $("#part3_4").click(function() {
+    $("#partThree").show();
+    $("#partOne").hide();
+    $("#partTwo").hide();
+    $("#partFive").hide();
+    $("#partSix").hide();
+  });
+
+  $("#part5").click(function() {
+    $("#partFive").show();
+    $("#partOne").hide();
+    $("#partTwo").hide();
+		$("#partThree").hide();
+    $("#partSix").hide();
+  });
+
+  $("#part6").click(function() {
+    $("#partSix").show();
+    $("#partOne").hide();
+    $("#partTwo").hide();
+    $("#partThree").hide();
+    $("#partFive").hide();
+  });
+
+
+});
